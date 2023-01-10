0e26317e39cff1a1953c1c55a6292469a9db45e9
OniHexx0ff <franciskaster86@gmail.com>
2023-01-10 02:55:34 -0300

    new layout

--- script.js
+++ script.js
@@ -5,78 +5,136 @@
   );
   const data = await res.json();
   const date = new Date();
-  const months = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
-  const monthText = document.querySelector('.calendar__header p')
+  const months = [
+    "Janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril",
+    "Maio",
+    "Junho",
+    "Julho",
+    "Agosto",
+    "Setembro",
+    "Outubro",
+    "Novembro",
+    "Dezembro",
+  ];
+  const monthText = document.querySelector(".calendar__header p");
   let day = date.getDate();
   let month = date.getMonth();
   let year = date.getFullYear();
-  let fullDate 
-  const showData = (fullDate) =>{
-    let dishes
-    try{
-    dishes = Object.entries(data[fullDate])
-    console.log(dishes)
+  let fullDate;
+  let dateFlag = false;
+
+  const showData = (fullDate) => {
+    let dishes;
+    try {
+      dishes = Object.entries(data[fullDate]);
+      console.log(data);
+    } catch {
+      dishes = [["Não foi possível acessar as informações deste dia", ""]];
     }
-    catch{
-       dishes = [["Não foi possível acessar as informações deste dia", ""]]
-    }
-    note.innerHTML = ""
+    note.innerHTML = "";
 
     dishes.forEach((dish) => {
       const dl = document.createElement("dl");
-      const items = dish[1].split(",")
+      const items = dish[1].split(",");
       note.appendChild(dl);
-      dl.insertAdjacentHTML("beforeend", `<dt>${dish[0].toLowerCase()}</dt>`)
-      
-      for (let i = 0; i < items.length - 1; i++){
+      dl.insertAdjacentHTML("beforeend", `<dt>${dish[0].toLowerCase()}</dt>`);
+
+      for (let i = 0; i < items.length - 1; i++) {
         dl.insertAdjacentHTML("beforeend", `<dd>${items.at(i)}, </dd>`);
       }
-       dl.insertAdjacentHTML("beforeend", `<dd>${items.at(-1)}</dd>`);
-      
+      dl.insertAdjacentHTML("beforeend", `<dd>${items.at(-1)}</dd>`);
     });
-  }
+  };
   const renderCalendar = () => {
-    // console.log(day,month,year)
-    monthText.innerHTML = months[month]
+    monthText.innerHTML = months[month];
 
-    const lastDay = new Date(year,month+1 ,0).getDate()
+    const lastDay = new Date(year, month + 1, 0).getDate();
 
-    const startWeekDay = new Date(year, month, 1).getDay()
-    const lastWeekDay = new Date(year, month,lastDay).getDay()
-    const caledar = document.querySelector('.calendar__body ol')
-    let dayTag = ""
-     console.log(lastDay)
+    const startWeekDay = new Date(year, month, 1).getDay();
+    const lastWeekDay = new Date(year, month, lastDay).getDay();
+    const calendar = document.querySelector(".calendar__body ol");
+    let dayTag = "";
 
-    for(let i = startWeekDay; i > 0; i--) dayTag += `<li class="inative">${lastDay -i + 1}</li>`
-    for(let i = 1; i <= lastDay; i++) {
-      if (i == day ) dayTag += `<li><span class="active">${i}</span></li>`
-     else dayTag += `<li>${i}</li>`
+    for (let i = startWeekDay; i > 0; i--)
+      dayTag += `<li class="inactive last">${lastDay - i + 1}</li>`;
+    for (let i = 1; i <= lastDay; i++) {
+      if (
+        i == date.getDate() &&
+        month == date.getMonth() &&
+        year == date.getFullYear()
+      )
+        dayTag += `<li><span class="current">${i}</span> <span class="active"> </span> </li>`;
+      else dayTag += `<li>${i} <span></span> </li>`;
     }
-    for(let i = lastWeekDay; i < 6; i++) dayTag += `<li class="inative">${i - lastWeekDay + 1}</li>`
+    for (let i = lastWeekDay; i < 6; i++)
+      dayTag += `<li class="inactive prev">${i - lastWeekDay + 1}</li>`;
 
-    caledar.innerHTML = dayTag
-  }
-  const dayClicked = (evt) =>{
-    const target = evt.target.closest('li')
-    const day = Number.isNaN(parseInt(target.innerHTML)) ? target.children[0].innerHTML : target.innerHTML
-    showData(`${day}/${month+1}/${year}`)
-  }
-  const changeMonth = (value) =>{
-    if (value < 0){
-      year = month > 0 ? year : year - 1
-      month = month > 0 ? month - 1 : 11
-    } 
-    else{
-      year = month < 11 ? year : year + 1
-      month = month < 11 ? month + 1 : 0
+    calendar.innerHTML = dayTag;
+    calendar.querySelectorAll("li:not(.prev)").forEach((li) => {
+      if (
+        (parseInt(li.innerText) < 25 && month < 11 && year == 2022) ||
+        (li.classList.contains("last") && month < 11 && year == 2022)
+      )
+        li.classList.add("outdated");
+    });
+  };
+  const dayClicked = (evt) => {
+    const target = evt.target.closest("li");
+    if (
+      target.classList.contains("inactive") ||
+      target.classList.contains("outdated")
+    )
+      return;
+    document
+      .querySelectorAll("li span")
+      .forEach((li) => li.classList.remove("active"));
+    target.children[target.children.length - 1].classList.add("active");
+    const day = target.innerText;
+    showData(`${day}/${month + 1}/${year}`);
+  };
+  const changeMonth = (value) => {
+    const startMonth = 10;
+    const startYear = 2022;
+    const backArrow = document.querySelector("svg:first-child");
+
+    if (value < 0) {
+      if (dateFlag) return;
+      year = month > 0 ? year : year - 1;
+      month = month > 0 ? month - 1 : 11;
+    } else {
+      year = month < 11 ? year : year + 1;
+      month = month < 11 ? month + 1 : 0;
     }
-    renderCalendar()
-  }
 
-  document.querySelector('.calendar__body ol').addEventListener('click', dayClicked)
-  document.querySelector('.calendar__header svg').addEventListener('click', () => {changeMonth(-1)})
-  document.querySelector('.calendar__header svg:last-child').addEventListener('click', () => {changeMonth(1)})
-  renderCalendar()
-  fullDate = `${day}/${month+1}/${year}`
-  showData(fullDate)
+    if (year == startYear && month - 1 < startMonth) {
+      backArrow.style.color = "#8080802e";
+      backArrow.style.cursosr = "default";
+      dateFlag = true;
+    } else {
+      backArrow.style.color = "black";
+      backArrow.style.cursosr = "pointer";
+      dateFlag = false;
+    }
+    renderCalendar();
+  };
+
+  document
+    .querySelector(".calendar__body ol")
+    .addEventListener("click", dayClicked);
+  document
+    .querySelector(".calendar__header svg")
+    .addEventListener("click", function () {
+      changeMonth(-1);
+    });
+  document
+    .querySelector(".calendar__header svg:last-child")
+    .addEventListener("click", function () {
+      changeMonth(1);
+    });
+  fullDate = `${day}/${month + 1}/${year}`;
+  renderCalendar();
+  showData(fullDate);
 })();
