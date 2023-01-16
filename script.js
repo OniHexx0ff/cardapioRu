c2915518fd8856c648f41851076167709633d656
OniHexx0ff <franciskaster86@gmail.com>
2023-01-16 01:59:51 -0300

    code and style changed

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-(async function () {
-  const note = document.querySelector(".note");
+
+async function fetchData(){
   const res = await fetch(
     "https://onihexx0ff.github.io/cardapioRu/cardapio.json"
   );
-  const data = await res.json();
-  const date = new Date();
+  return res.json();
+ 
+}
+
+function init (data) {
+  const today = new Date();
   const months = [
     "Janeiro",
     "Fevereiro",
@@ -21,22 +25,41 @@
   ];
   const monthText = document.querySelector(".calendar__header p");
   const yearText = document.querySelector(".calendar__header .year");
-  let day = date.getDate();
-  let month = date.getMonth();
-  let year = date.getFullYear();
-  let fullDate;
-  let dateFlag = false;
+  const note = document.querySelector(".note")
+  let calendarDate = today;
+  let fullDate =`${today.getDate()}/${today.getMonth() + 1}/${today.getFullYear()}`;
 
-  const showData = (fullDate) => {
+  const changeMonth = (function changeCalendar() {
+    let previousDate = new Date();
+    function computeDateChange(direction) {
+      const backButton = document.querySelector("button:first-child");
+      const firstRecord = new Date(2022, 10, 31).getTime();
+      previousDate =
+        direction > 0
+          ? new Date(previousDate.getFullYear(), previousDate.getMonth() + 1, 0)
+          : new Date(previousDate.getFullYear(), previousDate.getMonth(), 1);
+      previousDate.setDate(previousDate.getDate() + direction);
+      calendarDate = previousDate;
+
+      if (previousDate.getTime() < firstRecord) {
+        backButton.children[0].classList.add("arrow--disabled");
+        backButton.setAttribute("disabled", "");
+      } else {
+        backButton.children[0].classList.remove("arrow--disabled");
+        backButton.removeAttribute("disabled");
+      }
+      renderCalendar();
+    }
+    return computeDateChange;
+  })();
+  function showData() {
     let dishes;
     try {
       dishes = Object.entries(data[fullDate]);
-      console.log(data);
     } catch {
       dishes = [["Não foi possível acessar as informações deste dia", ""]];
     }
     note.innerHTML = "";
-
     dishes.forEach((dish) => {
       const dl = document.createElement("dl");
       const items = dish[1].split(",");
@@ -44,27 +67,36 @@
       dl.insertAdjacentHTML("beforeend", `<dt>${dish[0].toLowerCase()}</dt>`);
 
       for (let i = 0; i < items.length - 1; i++) {
-        dl.insertAdjacentHTML("beforeend", `<dd>${items.at(i)}, </dd>`);
+        dl.insertAdjacentHTML(
+          "beforeend",
+          `<dd>${items.at(i).toLowerCase()}, </dd>`
+        );
       }
-      dl.insertAdjacentHTML("beforeend", `<dd>${items.at(-1)}</dd>`);
+      dl.insertAdjacentHTML(
+        "beforeend",
+        `<dd>${items.at(-1).toLowerCase()}</dd>`
+      );
     });
   };
-  const renderCalendar = () => {
-    monthText.innerHTML = months[month];
-    yearText.innerHTML = `<p>${year}</p>`;
+  function renderCalendar ()  {
+    const year = calendarDate.getFullYear();
+    const month = calendarDate.getMonth();
     const lastDay = new Date(year, month + 1, 0).getDate();
     const startWeekDay = new Date(year, month, 1).getDay();
     const lastWeekDay = new Date(year, month, lastDay).getDay();
+
     const calendar = document.querySelector(".calendar__body ol");
+    monthText.innerHTML = months[month];
+    yearText.innerHTML = `<p>${year}</p>`;
     let dayTag = "";
 
     for (let i = startWeekDay; i > 0; i--)
       dayTag += `<li class="inactive last">${lastDay - i + 1}</li>`;
     for (let i = 1; i <= lastDay; i++) {
       if (
-        i == date.getDate() &&
-        month == date.getMonth() &&
-        year == date.getFullYear()
+        i == today.getDate() &&
+        month == today.getMonth() &&
+        year == today.getFullYear()
       )
         dayTag += `<li><span class="current">${i}</span> <span class="active"> </span> </li>`;
       else dayTag += `<li>${i} <span></span> </li>`;
@@ -74,79 +106,47 @@
 
     calendar.innerHTML = dayTag;
 
-    const lis =  calendar.querySelectorAll("li")
+    const lis = calendar.querySelectorAll("li");
     // outdated value
-    lis.forEach(li => {
-      let currentMonth = month + 1
-      const cellDate = `${parseInt(li.innerText)}/${currentMonth}/${year}`
-      console.log(cellDate)
-      if (!data[cellDate] && !li.classList.contains('prev') && !li.classList.contains('last')) li.classList.add('outdated')
-    })
-    // lis.forEach((li) => {
-    //   if (month < 11 && year == 2022 && !li.classList.contains("prev")) {
-    //     if (parseInt(li.innerText) < 25 || li.classList.contains("last"))
-    //       li.classList.add("outdated");
-    //   }
-    // });
-
-
-
+    lis.forEach((li) => {
+      const cellDate = `${parseInt(li.innerText)}/${month + 1}/${year}`;
+      if (
+        !data[cellDate] &&
+        !li.classList.contains("prev") &&
+        !li.classList.contains("last")
+      )
+        li.classList.add("outdated");
+    });
   };
-  const dayClicked = (evt) => {
+  function dayClicked (evt) {
     const target = evt.target.closest("li");
     if (
       target.classList.contains("inactive") ||
       target.classList.contains("outdated")
     )
-      return;
-    document
-      .querySelectorAll("li span")
-      .forEach((li) => li.classList.remove("active"));
+      return; 
+     document.querySelector(".active").classList.remove("active");
     target.children[target.children.length - 1].classList.add("active");
-    const day = target.innerText;
-    showData(`${day}/${month + 1}/${year}`);
-  };
-  const changeMonth = (value) => {
-    const startMonth = 10;
-    const startYear = 2022;
-    const backArrow = document.querySelector("svg:first-child");
-
-    if (value < 0) {
-      if (dateFlag) return;
-      year = month > 0 ? year : year - 1;
-      month = month > 0 ? month - 1 : 11;
-    } else {
-      year = month < 11 ? year : year + 1;
-      month = month < 11 ? month + 1 : 0;
-    }
-
-    if (year == startYear && month - 1 < startMonth) {
-      backArrow.style.color = "#8080802e";
-      backArrow.style.cursosr = "default";
-      dateFlag = true;
-    } else {
-      backArrow.style.color = "black";
-      backArrow.style.cursosr = "pointer";
-      dateFlag = false;
-    }
-    renderCalendar();
+    fullDate = `${target.innerText}/${today.getMonth() + 1}/${today.getFullYear()}`
+    showData();
   };
 
   document
     .querySelector(".calendar__body ol")
     .addEventListener("click", dayClicked);
   document
-    .querySelector(".calendar__header svg")
+    .querySelector(".calendar__header button")
     .addEventListener("click", function () {
       changeMonth(-1);
     });
   document
-    .querySelector(".calendar__header svg:last-child")
+    .querySelector(".calendar__header button:last-child")
     .addEventListener("click", function () {
       changeMonth(1);
     });
 
-  fullDate = `${day}/${month + 1}/${year}`;
   renderCalendar();
-  showData(fullDate);
-})();
+  showData();
+}
+
+fetchData().then(data => init(data))
